27cd31f9922773f68f53b095881e022d5451fdae
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2024-10-21 10:43:15 +0300

    first commit

---
+++ Test/Utils/PasswordEncryption.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.Utils
+{
+    public class PasswordEncryption
+    {
+        public static string ToHash(string password)
+        {
+            return string.Empty;
+        }
+    }
+}
