ab275e578def8dc43c5e679c945467e688b15d81
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2024-10-21 17:36:15 +0300

    add session management

---
+++ Test/Utils/SessionService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using Test.Models;
+using Test.Models.Exceptions;
+
+namespace Test.Utils
+{
+    public class SessionService
+    {
+        public static bool IsConnected(string session)
+        {
+            if (string.IsNullOrEmpty(session))
+                throw new UserNotConnectedException();
+
+            LoginModel user = JsonSerializer.Deserialize<LoginModel>(session) ?? throw new UserNotConnectedException();
+
+            if(user.UserId == 0)
+                throw new UserNotConnectedException();
+
+
+
+            return true;
+        }
+    }
+}
