29b38e30bb2ec35c3965837b5926907a7f08ab25
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2024-10-21 17:38:27 +0300

    Refactorize session management

--- Test/Utils/SessionService.cs
+++ Test/Utils/SessionService.cs
@@ -6,19 +6,22 @@ namespace Test.Utils
 {
     public class SessionService
     {
-        public static bool IsConnected(string session)
+        public static void IsConnected(string session)
         {
             if (string.IsNullOrEmpty(session))
                 throw new UserNotConnectedException();
 
-            LoginModel user = JsonSerializer.Deserialize<LoginModel>(session) ?? throw new UserNotConnectedException();
+            LoginModel user = GetUserData(session);
 
             if(user.UserId == 0)
                 throw new UserNotConnectedException();
 
+        }
 
-
-            return true;
+        public static LoginModel GetUserData(string session) 
+        {
+            LoginModel user = JsonSerializer.Deserialize<LoginModel>(session) ?? throw new UserNotConnectedException();
+            return user;
         }
     }
 }
