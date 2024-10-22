ee8e7f6aa708c8c256965c5fe2d5ab6877652f6f
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2024-10-22 10:38:05 +0300

    delete the connection string for the database

--- InsertUser/Program.cs
+++ InsertUser/Program.cs
@@ -1,7 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using Test.Utils;
 
-MySqlConnection connection = new("Server=sql.freedb.tech;Port=3306;Database=freedb_test_cure;User Id=freedb_manitra;Password=#TRfJ2KZtp7n*UP;");
+MySqlConnection connection = new("");
 
 Console.WriteLine("Enter username :");
 string? username = Console.ReadLine();
